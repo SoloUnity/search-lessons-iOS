e2b190eabb8f142ef46932ffb8d878e2497cff92
SoloUnity <GordonAccount@yahoo.com>
2022-07-05 14:53:37 -0400

    Initial Commit

---
+++ Module 5 Final Challenge/Module_5_Final_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module_5_Final_ChallengeApp.swift
+//  Module 5 Final Challenge
+//
+//  Created by Gordon Ng on 2022-07-05.
+//
+
+import SwiftUI
+
+@main
+struct Module_5_Final_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
